be70d16acbbb6281f32fdc7fe22cc8f37158de7b
maradonamania <80200804+maradonamania@users.noreply.github.com>
2021-08-19 13:28:54 +0900

    変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,3 +114,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+server '52.192.10.151', user: 'ec2-user', roles: %w{app db web}
+
