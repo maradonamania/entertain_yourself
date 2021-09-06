e274b3093f08388a8013c027c7b5e7c9f83bd2c7
maradonamania <80200804+maradonamania@users.noreply.github.com>
2021-09-06 12:39:41 +0900

    header/activehash修正

--- app/models/expected_time.rb
+++ app/models/expected_time.rb
@@ -1,11 +1,14 @@
 class ExpectedTime < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '1〜2時間' },
-    { id: 2, name: '2〜3時間' },
-    { id: 3, name: '4〜5時間' },
-    { id: 4, name: '半日' },
-    { id: 5, name: '1日以上' }
+    { id: 1, name: '30分' },
+    { id: 2, name: '1〜2時間' },
+    { id: 3, name: '2〜3時間' },
+    { id: 4, name: '3〜4時間' },
+    { id: 5, name: '4〜5時間' },
+    { id: 6, name: '半日' },
+    { id: 7, name: '1日' },
+    { id: 8, name: '2日以上' }
   ]
   include ActiveHash::Associations
   has_many :plans
